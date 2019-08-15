1955d760e5fc10ab798f3a7e7dac4a4f2a85a93d
Alex <akuzin20@gmail.com>
2019-08-15 20:02:15 +0300

    init v2 refactoring: js->jsx, style, component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/homepage'
+import HomepageComponent from './pages/homepage/homepage.component'
 
 function App() {
     return (
         <div >
-            <HomePage/>
+            <HomepageComponent/>
         </div>
     );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MainMenu from '../../components/mainmenu/main-menu.component';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <MainMenu />
+    </div>
+);
+
+export default HomePage;
