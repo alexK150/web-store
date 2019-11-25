eedaad05d145056f7e356cd05f8d67a7bd0bec22
Alex <akuzin20@gmail.com>
2019-11-25 20:03:21 +0300

    init v7 added Header Component & links to Home Shop page and Home

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {Route, Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/Shop.component';
 
 import './App.css';
+import Header from './components/header/Header.component';
 
 const App = () => {
     return (
         <div>
+            <Header/>
             <Switch>
                 <Route exact path='/' component={Homepage}/>
                 <Route exact path='/shop' component={ShopPage}/>
