06adfce6c563c6831bc98d0180de9b42c28aabc3
Alex <akuzin20@gmail.com>
2019-11-26 15:42:48 +0300

    init v8 added SignIn Component & SignInAndSignUp page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Homepage from './pages/homepage/Homepage.component';
 import {Route, Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/Shop.component';
+import Header from './components/header/Header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/SignInAndSignUp.component';
 
 import './App.css';
-import Header from './components/header/Header.component';
 
 const App = () => {
     return (
@@ -13,6 +14,7 @@ const App = () => {
             <Switch>
                 <Route exact path='/' component={Homepage}/>
                 <Route exact path='/shop' component={ShopPage}/>
+                <Route exact path='/signin' component={SignInAndSignUp}/>
             </Switch>
         </div>
     );
