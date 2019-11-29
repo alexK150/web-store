f3dc5a7f562dd0986abdbcd19d5279c5c557a87f
Alex <akuzin20@gmail.com>
2019-11-29 13:57:11 +0300

    init v19 Added REDUX to project and user reducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+
+import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
+    <Provider>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'));
 
 serviceWorker.unregister();
